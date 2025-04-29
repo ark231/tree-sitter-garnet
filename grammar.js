b30cd5a73431df633a9e70f6640014c3da468d82
ark231 <arklp20909@gmail.com>
2025-04-29 18:47:27 +0900

    add: 関数定義の大枠

--- grammar.js
+++ grammar.js
@@ -12,6 +12,44 @@ module.exports = grammar({
 
   rules: {
     // TODO: add the actual grammar rules
-    source_file: $ => "hello"
+    source_file: $ => repeat(choice($._definition, $._declaration)),
+    _definition: $ => choice(
+      $.function_definition
+    ),
+    _declaration: $ => choice(
+      // TODO: add variable decl
+    ),
+    function_definition: $ => seq(
+      "func",
+      $.identifier,
+      $.parameter_list,
+      "->",
+      $._type,
+      $.block
+    ),
+    identifier: $ => /[a-zA-Z][a-zA-Z0-9_]*/,
+    parameter_list: $ => seq(
+      "(",
+      // TODO: implement
+      ")"
+    ),
+    _type: $ => choice(
+      "u8",
+      "i8",
+      "u16",
+      "i16",
+      "u32",
+      "i32",
+      "u64",
+      "i64",
+      "f32",
+      "f64",
+      $.identifier
+    ),
+    block: $ => seq(
+      "{",
+      // TODO: implment
+      "}"
+    )
   }
 });
